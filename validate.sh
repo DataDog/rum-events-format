b01e4821e5dd59e8fab780eae68329e7b4deabf1
Marius Constantin <marius.constantin@datadoghq.com>
2022-06-28 17:04:21 +0200

    Add Shape and Text json schemas for Mobile Session Replay (#65)
    * Add Shape and Text json schemas for Mobile Session Replay
    
    * Add rum-session-replay as codeowners

--- validate.sh
+++ validate.sh
@@ -2,16 +2,32 @@
 
 line='----------------------------------------'
 
-for sample in samples/*/*.json; do
-  printf "Validating %s %s " "$sample" "${line:${#sample}}"
-  result=$(python validate.py "$sample" rum-events-format.json 2>&1)
-  status=$?
-  if [ $status -ne 0 ]
-  then
-    echo "❌"
-    echo "$result"
-    exit $status
-  else
-    echo "✅"
-  fi
+declare -a sample_schema_folders=(
+  "rum-schema"
+  "session-replay-mobile-schema"
+  )
+
+declare -a event_formats_for_samples=(
+  "rum-events-format.json"
+  "session-replay-mobile-events-format.json"
+  )
+
+index = 0
+for schema_folder in "${sample_schema_folders[@]}"; do 
+  printf "Validating samples in %s with %s" "$schema_folder" "${event_formats_for_samples[index]}"
+  printf "\n"
+  for sample in samples/$schema_folder/*/*.json; do
+    printf "Validating %s %s " "$sample" "${line:${#sample}}"
+    result=$(python validate.py "$sample" "${event_formats_for_samples[index]}" 2>&1)
+    status=$?
+    if [ $status -ne 0 ]
+    then
+      echo "❌"
+      echo "$result"
+      exit $status
+    else
+      echo "✅"
+    fi
+  done
+  ((index=index+1))
 done
