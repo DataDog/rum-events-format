536c48edb1c44b914568991bdcaf1f509be46969
William Lacroix <william.lacroix@datadoghq.com>
2022-08-09 16:34:08 +0100

    update build

--- lib/generated/rum.d.ts
+++ lib/generated/rum.d.ts
@@ -430,7 +430,7 @@ export declare type RumResourceEvent = CommonProperties & ActionChildProperties 
          */
         readonly trace_id?: string;
         /**
-         * tracing sample rate in integer format
+         * tracing sample rate in decimal format
          */
         readonly rule_psr?: number;
         [k: string]: unknown;
