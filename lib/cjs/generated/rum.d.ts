c8d1e20038d90a4c65b3a3729aef53db71b62e8c
Hamza Kadiri <hamza.kadiri@datadoghq.com>
2023-06-21 16:37:20 +0200

    properties are non optional

--- lib/cjs/generated/rum.d.ts
+++ lib/cjs/generated/rum.d.ts
@@ -760,19 +760,19 @@ export declare type RumViewEvent = CommonProperties & {
             /**
              * Distance between the top and the lowest point reached on this view (in pixels)
              */
-            readonly max_depth?: number;
+            readonly max_depth: number;
             /**
              * Page scroll height (total height) when the maximum scroll depth was reached for this view (in pixels)
              */
-            readonly max_depth_scroll_height?: number;
+            readonly max_depth_scroll_height: number;
             /**
              * Page scroll top (scrolled distance) when the maximum scroll depth was reached for this view (in pixels)
              */
-            readonly max_depth_scroll_top?: number;
+            readonly max_depth_scroll_top: number;
             /**
              * Duration between the view start and the scroll event that reached the maximum scroll depth for this view (in nanoseconds)
              */
-            readonly max_depth_time?: number;
+            readonly max_depth_time: number;
             [k: string]: unknown;
         };
         [k: string]: unknown;
