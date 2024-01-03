c717d074800883a2978421acfbf1b30cc1b3af50
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2024-01-03 18:07:29 +0100

    ♻️ update validate.mjs script design

--- scripts/validate.mjs
+++ scripts/validate.mjs
@@ -2,40 +2,46 @@ import path from 'path'
 import Ajv from 'ajv'
 import { readdirSync, readFileSync } from 'fs'
 
-const ajv = new Ajv()
-
 const SAMPLES_DIRECTORY = './samples'
 const SCHEMAS_DIRECTORY = './schemas'
 
-forEachFile(SCHEMAS_DIRECTORY, addSchema)
-forEachFile(SAMPLES_DIRECTORY, validateSample)
+validateSchemasIds()
+validateSamples()
 
-function addSchema(schemaPath) {
-  const schema = readJson(schemaPath)
-
-  // We need to be careful about schema ids because they need to:
-  // * be unique, or else Ajv will throw
-  // * represent a path, as Ajv will use it to resolve $refs
-  // Here, we make sure that both requirements are respected.
-  const schemaId = computeSchemaIdFromSchemaPath(schemaPath)
-  if (schema.$id !== schemaId) {
-    console.log(`⚠️ Schema ${schemaPath} $id should be ${schemaId}`)
-    process.exitCode = 1
-  }
-
-  ajv.addSchema(schema)
+if (process.exitCode !== 0 && process.exitCode !== undefined) {
+  console.log('❌ Some validation errors were found')
 }
 
-function validateSample(samplePath) {
-  const schemaId = computeSchemaIdFromSamplePath(samplePath)
-  const valid = ajv.validate(schemaId, readJson(samplePath))
-  if (valid) {
-    console.log(`✅ ${samplePath}`)
-  } else {
-    console.log(`❌ ${samplePath} is not valid against ${schemaId}:`)
-    console.log(`   - ${ajv.errorsText(undefined, { separator: '\n   - ' })}`)
-    process.exitCode = 1
-  }
+function validateSchemasIds() {
+  forEachFile(SCHEMAS_DIRECTORY, (schemaPath) => {
+    const schema = readJson(schemaPath)
+
+    // We need to be careful about schema ids because they need to:
+    // * be unique, or else Ajv will throw
+    // * represent a path, as Ajv will use it to resolve $refs
+    // Here, we make sure that both requirements are respected.
+    const schemaId = computeSchemaIdFromSchemaPath(schemaPath)
+    if (schema.$id !== schemaId) {
+      console.log(`⚠️ Schema ${schemaPath} $id should be ${schemaId}`)
+      process.exitCode = 1
+    }
+  })
+}
+
+function validateSamples() {
+  const ajv = new Ajv()
+  forEachFile(SCHEMAS_DIRECTORY, (schemaPath) => ajv.addSchema(readJson(schemaPath)))
+  forEachFile(SAMPLES_DIRECTORY, (samplePath) => {
+    const schemaId = computeSchemaIdFromSamplePath(samplePath)
+    const valid = ajv.validate(schemaId, readJson(samplePath))
+    if (valid) {
+      console.log(`✅ ${samplePath}`)
+    } else {
+      console.log(`❌ ${samplePath} is not valid against ${schemaId}:`)
+      console.log(`   - ${ajv.errorsText(undefined, { separator: '\n   - ' })}`)
+      process.exitCode = 1
+    }
+  })
 }
 
 function computeSchemaIdFromSchemaPath(schemaPath) {
