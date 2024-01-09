4d199f06941870b70bc05c45d240f5cb62e95a4a
Benoît <benoit.zugmeyer@datadoghq.com>
2024-01-09 10:14:38 +0100

    Validate objects properties case (#176)
    * ♻️ update validate.mjs script design
    
    * ✅ validate objects properties case
    
    * 👌 remove ternary
    
    * 👌 display the casing convention if a casing error occurs
    
    * 👌 update node and add integration tests for 'validate.mjs'

--- scripts/validate.mjs
+++ scripts/validate.mjs
@@ -2,40 +2,87 @@ import path from 'path'
 import Ajv from 'ajv'
 import { readdirSync, readFileSync } from 'fs'
 
-const ajv = new Ajv()
-
 const SAMPLES_DIRECTORY = './samples'
 const SCHEMAS_DIRECTORY = './schemas'
 
-forEachFile(SCHEMAS_DIRECTORY, addSchema)
-forEachFile(SAMPLES_DIRECTORY, validateSample)
+validateSchemasObjectsPropertiesCase()
+validateSchemasIds()
+validateSamples()
 
-function addSchema(schemaPath) {
-  const schema = readJson(schemaPath)
-
-  // We need to be careful about schema ids because they need to:
-  // * be unique, or else Ajv will throw
-  // * represent a path, as Ajv will use it to resolve $refs
-  // Here, we make sure that both requirements are respected.
-  const schemaId = computeSchemaIdFromSchemaPath(schemaPath)
-  if (schema.$id !== schemaId) {
-    console.log(`⚠️ Schema ${schemaPath} $id should be ${schemaId}`)
-    process.exitCode = 1
-  }
-
-  ajv.addSchema(schema)
+if (process.exitCode !== 0 && process.exitCode !== undefined) {
+  console.log('❌ Some validation errors were found')
 }
 
-function validateSample(samplePath) {
-  const schemaId = computeSchemaIdFromSamplePath(samplePath)
-  const valid = ajv.validate(schemaId, readJson(samplePath))
-  if (valid) {
-    console.log(`✅ ${samplePath}`)
-  } else {
-    console.log(`❌ ${samplePath} is not valid against ${schemaId}:`)
-    console.log(`   - ${ajv.errorsText(undefined, { separator: '\n   - ' })}`)
-    process.exitCode = 1
+function validateSchemasObjectsPropertiesCase() {
+  // Some properties don't follow the convention. Ideally they should be fixed in the future.
+  const CASING_EXCEPTIONS = new Map([
+    [
+      `${SCHEMAS_DIRECTORY}/session-replay/common/_common-segment-metadata-schema.json`,
+      ['records_count', 'index_in_view', 'has_full_snapshot'],
+    ],
+    [`${SCHEMAS_DIRECTORY}/session-replay/common/focus-record-schema.json`, ['has_focus']],
+    [`${SCHEMAS_DIRECTORY}/rum/resource-schema.json`, ['operationType', 'operationName']],
+  ])
+
+  let displayConvention = false
+
+  forEachFile(SCHEMAS_DIRECTORY, (schemaPath) => {
+    const schema = readJson(schemaPath)
+
+    // RUM and telemetry schemas object properties should be snake_case, other schemas objects should
+    // be camelCase
+    const shouldBeSnakeCase =
+      schemaPath.startsWith(`${SCHEMAS_DIRECTORY}/rum/`) || schemaPath.startsWith(`${SCHEMAS_DIRECTORY}/telemetry/`)
+
+    const caseExceptions = CASING_EXCEPTIONS.get(schemaPath) || []
+
+    forEachObjectProperty(schema, (key) => {
+      const isCorrectCase = shouldBeSnakeCase ? isSnakeCase(key) : isCamelCase(key)
+      if (!isCorrectCase && !caseExceptions.includes(key)) {
+        console.log(`❌ Schema ${schemaPath} property ${key} is not ${shouldBeSnakeCase ? 'snake_case' : 'camelCase'}`)
+        displayConvention = true
+        process.exitCode = 1
+      }
+    })
+  })
+
+  if (displayConvention) {
+    console.log(
+      'ℹ️  RUM and telemetry schemas object properties should be snake_case, other schemas objects should be camelCase'
+    )
   }
+}
+
+function validateSchemasIds() {
+  forEachFile(SCHEMAS_DIRECTORY, (schemaPath) => {
+    const schema = readJson(schemaPath)
+
+    // We need to be careful about schema ids because they need to:
+    // * be unique, or else Ajv will throw
+    // * represent a path, as Ajv will use it to resolve $refs
+    // Here, we make sure that both requirements are respected.
+    const schemaId = computeSchemaIdFromSchemaPath(schemaPath)
+    if (schema.$id !== schemaId) {
+      console.log(`❌ Schema ${schemaPath} $id should be ${schemaId}`)
+      process.exitCode = 1
+    }
+  })
+}
+
+function validateSamples() {
+  const ajv = new Ajv()
+  forEachFile(SCHEMAS_DIRECTORY, (schemaPath) => ajv.addSchema(readJson(schemaPath)))
+  forEachFile(SAMPLES_DIRECTORY, (samplePath) => {
+    const schemaId = computeSchemaIdFromSamplePath(samplePath)
+    const valid = ajv.validate(schemaId, readJson(samplePath))
+    if (valid) {
+      console.log(`✅ ${samplePath}`)
+    } else {
+      console.log(`❌ ${samplePath} is not valid against ${schemaId}:`)
+      console.log(`   - ${ajv.errorsText(undefined, { separator: '\n   - ' })}`)
+      process.exitCode = 1
+    }
+  })
 }
 
 function computeSchemaIdFromSchemaPath(schemaPath) {
@@ -63,4 +110,35 @@ function forEachFile(directoryPath, callback) {
 
 function readJson(filePath) {
   return JSON.parse(readFileSync(filePath, 'utf8'))
+}
+
+/**
+ * Iterates over each properties of objects specified in the provided JSON schema.
+ */
+function forEachObjectProperty(schema, callback) {
+  if (Array.isArray(schema)) {
+    // traverse arrays
+    for (const value of schema) {
+      forEachObjectProperty(value, callback)
+    }
+  } else if (typeof schema === 'object' && schema !== null) {
+    // traverse objects
+    for (const value of Object.values(schema)) {
+      forEachObjectProperty(value, callback)
+    }
+
+    if (schema.type === 'object' && schema.properties) {
+      for (const [key, value] of Object.entries(schema.properties)) {
+        callback(key, value)
+      }
+    }
+  }
+}
+
+function isSnakeCase(str) {
+  return /^[a-z0-9_]+$/.test(str)
+}
+
+function isCamelCase(str) {
+  return /^[a-z0-9][A-Za-z0-9]*$/.test(str)
 }
