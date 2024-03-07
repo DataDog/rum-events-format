4cec89484f7965239d3196022be8fc5f30f4e724
Thomas Lebeau <thomas.lebeau@datadoghq.com>
2024-03-07 20:05:37 +0100

    renam csp_disposition to csp.disposition

--- lib/cjs/generated/rum.d.ts
+++ lib/cjs/generated/rum.d.ts
@@ -348,9 +348,15 @@ export declare type RumErrorEvent = CommonProperties & ActionChildProperties & V
             [k: string]: unknown;
         };
         /**
-         * In the context of CSP errors, indicates how the violated policy is configured to be treated by the user agent.
+         * Content Security Violation properties
          */
-        readonly csp_disposition?: 'enforce' | 'report';
+        readonly csp?: {
+            /**
+             * In the context of CSP errors, indicates how the violated policy is configured to be treated by the user agent.
+             */
+            readonly disposition?: 'enforce' | 'report';
+            [k: string]: unknown;
+        };
         [k: string]: unknown;
     };
     /**
