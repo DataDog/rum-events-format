06f9d0e270df9e56feaf5a1315202276b793b0b6
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2025-12-02 14:56:29 +0100

    add ProfileEvent and ProfileEventPayload

--- scripts/validate.mjs
+++ scripts/validate.mjs
@@ -23,6 +23,8 @@ function validateSchemasObjectsPropertiesCase() {
     ],
     [`${SCHEMAS_DIRECTORY}/session-replay/common/focus-record-schema.json`, ['has_focus']],
     [`${SCHEMAS_DIRECTORY}/rum/resource-schema.json`, ['operationType', 'operationName']],
+    [`${SCHEMAS_DIRECTORY}/profiling/profile-event-attributes-schema.json`, ['long_task']],
+    [`${SCHEMAS_DIRECTORY}/profiling/profile-event-payload-schema.json`, ['wall-time.json']],
   ])
 
   let displayConvention = false
