b50e108754167d3d4776ff40789f2e8e811270aa
Aymeric <aymeric.mortemousque@datadoghq.com>
2026-05-28 10:36:57 +0200

    Fix telemetry action id type (#287)

--- lib/cjs/generated/telemetry.d.ts
+++ lib/cjs/generated/telemetry.d.ts
@@ -579,7 +579,7 @@ export interface CommonTelemetryProperties {
         /**
          * UUID of the action
          */
-        id: string;
+        readonly id: string | string[];
         [k: string]: unknown;
     };
     /**
