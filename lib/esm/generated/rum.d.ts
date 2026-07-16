9b9b48b05d13b42e04c3cbf8ee8ba79e2c7552aa
Barbora Plasovska <barbora.plasovska@datadoghq.com>
2026-07-16 10:37:51 +0200

    RUM-17207 Add local_cache_hit field to resource event schema

--- lib/esm/generated/rum.d.ts
+++ lib/esm/generated/rum.d.ts
@@ -779,6 +779,10 @@ export type RumResourceEvent = CommonProperties & ActionChildProperties & ViewCo
          */
         readonly delivery_type?: 'cache' | 'navigational-prefetch' | 'other';
         /**
+         * Mobile-only: indicates the resource was served from the device's local cache. Only ever sent as `true`; absence means not a local cache hit.
+         */
+        readonly local_cache_hit?: boolean;
+        /**
          * The provider for this resource
          */
         readonly provider?: {
